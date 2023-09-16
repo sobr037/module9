034daa4479341ab7c837d78ca69e0a45e1bc344b
Your Usernamesobr037 <samobrien49@gmail.com>
2023-09-16 14:36:27 +1200

    Adding Ex1

---
+++ ex1/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+let dbConnect = require("./dbConnect");
+const app = express();
+require("dotenv").config();
+
+// Parse requests of content-type - application/json
+app.use(express.json());
+
+// Define your userRoutes before other routes
+let userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my MongoDB application." });
+});
+
+// Set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
